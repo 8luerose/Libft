c02c19e8e354b9abaa1dabe9d28124a96e1844f5
Taehyun Kwon <taehkwon@cx2r6s4.42seoul.kr>
2023-03-19 22:51:34 +0900

    ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,27 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: taehkwon <taehkwon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/19 20:36:21 by taehkwon          #+#    #+#             */
-/*   Updated: 2023/03/19 22:40:55 by taehkwon         ###   ########.fr       */
+/*   Created: 2023/03/19 22:37:41 by taehkwon          #+#    #+#             */
+/*   Updated: 2023/03/19 22:45:45 by taehkwon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <string.h>
+void	ft_bzero(void *s, size_t n)
+{
+	unsigned char	*p;
 
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-
-#endif
+	p = (unsigned char *)s;
+	while (n--)
+	{
+		*p = 0;
+		p++;
+	}
+}
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,27 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: taehkwon <taehkwon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/19 20:36:21 by taehkwon          #+#    #+#             */
-/*   Updated: 2023/03/19 22:40:55 by taehkwon         ###   ########.fr       */
+/*   Created: 2023/03/19 22:46:30 by taehkwon          #+#    #+#             */
+/*   Updated: 2023/03/19 22:51:05 by taehkwon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <string.h>
-
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-
-#endif
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	
+}
