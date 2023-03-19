fef95a69ebe312389dafdc9207540ffcdd5bfca1
Taehyun Kwon <taehkwon@cx2r6s4.42seoul.kr>
2023-03-19 22:37:10 +0900

    memset

---
+++ ft_memset.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taehkwon <taehkwon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/19 20:45:01 by taehkwon          #+#    #+#             */
+/*   Updated: 2023/03/19 22:34:28 by taehkwon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*p;
+
+	p = (unsigned char *)b;
+	while (len--)
+	{
+		*p = (unsigned char)c;
+		p++;
+	}
+	return (b);
+}
